4ae5b279d2d0d7737b36fe5b507bb8e82076d85d
Iarla Soares <iarla3831@gmail.com>
2024-03-15 13:53:03 -0300

    ta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,5 +6,5 @@
 <h1>Bem-vindo Escráviario, <br></h1>
 <button><a href="/login"><br>Acessar sua folha de ponto.</a></button>
 <button><a href="/produtos"><br><br>Produtos</a></button>
-<img src="/public/img/sapinho.png alt=sapo">
+
 @endsection
--- storage/framework/views/7f64ab75cd331748d3bc9311b184cf24.php
+++ storage/framework/views/7f64ab75cd331748d3bc9311b184cf24.php
@@ -4,7 +4,7 @@
 <h1>Bem-vindo Escráviario, <br></h1>
 <button><a href="/login"><br>Acessar sua folha de ponto.</a></button>
 <button><a href="/produtos"><br><br>Produtos</a></button>
-<img src="/public/img/sapinho.png alt=sapo">
+<img src="<?php echo e(asset('../../public/img/sapinho.png')); ?>" alt=sapo>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\tools\php\iarlla\resources\views/welcome.blade.php ENDPATH**/ ?>
