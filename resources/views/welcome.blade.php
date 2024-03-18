7c0b0c335bcb345d5bbf1b96f5e503d15806fd18
Iarla Soares <iarla3831@gmail.com>
2024-03-18 15:20:04 -0300

    aula 14

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,28 @@
 @section('title', 'Folha de ponto')
 @section('content')
 
-<h1>Bem-vindo Escráviario, <br></h1>
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+        <div class="card col-md-3">
+            <img src="{{ asset('img/119.jpg') }}" alt="{{ $event->title }}">
+            <div class="card-body">
+                <p class="card-date">10/09/2020</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-participants">X Participantes</p>
+                <a href="#" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
--- storage/framework/views/7f64ab75cd331748d3bc9311b184cf24.php
+++ storage/framework/views/7f64ab75cd331748d3bc9311b184cf24.php
@@ -1,7 +1,29 @@
 <?php $__env->startSection('title', 'Folha de ponto'); ?>
 <?php $__env->startSection('content'); ?>
 
-<h1>Bem-vindo Escráviario, <br></h1>
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        <?php $__currentLoopData = $events; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $event): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <div class="card col-md-3">
+            <img src="<?php echo e(asset('img/119.jpg')); ?>" alt="<?php echo e($event->title); ?>">
+            <div class="card-body">
+                <p class="card-date">10/09/2020</p>
+                <h5 class="card-title"><?php echo e($event->title); ?></h5>
+                <p class="card-participants">X Participantes</p>
+                <a href="#" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </div>
+</div>
 
 <?php $__env->stopSection(); ?>
 
